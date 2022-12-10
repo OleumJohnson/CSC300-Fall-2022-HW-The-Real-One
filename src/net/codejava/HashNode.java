a7cac5cff00ad6f59bae3f46e3f8c553aa125045
OleumJohnson <102564035+OleumJohnson@users.noreply.github.com>
2022-12-09 18:36:45 -0600

    CSC 300 HW 13
    The thirteenth and final homework for CSC300, Fall Semester 2022.

--- src/net/codejava/HashNode.java
+++ src/net/codejava/HashNode.java
@@ -9,6 +9,8 @@ public class HashNode {
     public String value;
     final int hashCode;
     HashNode next;
+    HashNode deleter = null;
+    //int height = 20;
  
     HashNode(String key, String value, int hashCode) {
         this.key = key;
@@ -17,15 +19,18 @@ public class HashNode {
     }
 }
  
-class Map {
+//this isn't a mispelling, eclipse just hates when I name things HashMap
+class HasMap {
     public ArrayList<HashNode> bucketArray;
     public int numOfBuckets;
-    public int size;
+    public static int size;
+    int height = 10;
+    
  
-    public Map(String key, int hashCode) {
+    public HasMap(String key, int hashCode) {
         bucketArray = new ArrayList<>();
         numOfBuckets = 10;
-        size = 0;
+        //size;
  
         for (int i = 0; i < numOfBuckets; i++) {
             bucketArray.add(null);
@@ -39,7 +44,7 @@ class Map {
     public boolean isEmpty() {
     	return size() == 0;
     }
-     
+     /*
       private final int hashCode (String key) {
         return Objects.hashCode(key);
     }
@@ -51,8 +56,21 @@ class Map {
         index = index < 0 ? index * -1 : index;
         return index;
     }
- 
+ 	*/
     public String remove(String key) {
+    	int hashIndex = Integer.parseInt(key) % height;
+    	
+    	while(bucketArray.get(hashIndex) != null) {
+    		if(bucketArray.get(hashIndex).key.equals(key)) {
+    			bucketArray.remove(hashIndex);
+    			return null;
+    		}
+    		hashIndex++;
+    		hashIndex %= height;
+    	}
+    	return null;
+    	
+    	/*
         int bucketIndex = getBucketIndex(key);
         int hashCode = hashCode(key);
         HashNode head = bucketArray.get(bucketIndex);
@@ -80,25 +98,48 @@ class Map {
             bucketArray.set(bucketIndex, head.next);
         }
         return head.value;
+        */
     }
  
     public String get(String key)
     {
-        int bucketIndex = getBucketIndex(key);
-        int hashCode = hashCode(key);
-       
-        HashNode head = bucketArray.get(bucketIndex);
+        //int bucketIndex = getBucketIndex(key);
+        //int hashCode = hashCode(key);
+        int hashIndex = (Integer.parseInt(key) % height);
+        //HashNode head = bucketArray.get(bucketIndex);
+        int count = 0;
  
-        while (head != null) {
-            if (head.key.equals(key) && head.hashCode == hashCode) {
-                return head.value;
+        while (bucketArray.get(hashIndex) != null) {
+        	if(count + 1 > height) {
+        		break;
+        	}
+            if (bucketArray.get(hashIndex).key.equals(key)) {
+                return bucketArray.get(hashIndex).value;
             }
-            head = head.next;
+            hashIndex++;
+            hashIndex %= height;
         }
         return null;
     }
  
     public void add(String key, String value) {
+    	HashNode placeholder = new HashNode(key, value, this.hashCode());
+    	placeholder.key = key;
+    	placeholder.value = value;
+    	int hashIndex = Integer.parseInt(key) % height;
+    	
+    	while(bucketArray.get(hashIndex) != null && bucketArray.get(hashIndex).key != key && bucketArray.get(hashIndex).key != null) {
+    		hashIndex++;
+    		hashIndex %= height;
+    	}
+    	
+    	if(bucketArray.get(hashIndex) == null || bucketArray.get(hashIndex).key == null) {
+    		size++;
+    	}
+    	    	
+    	//bucketArray.set(hashIndex, placeholder);
+    	
+    	/*
         int bucketIndex = getBucketIndex(key);
         int hashCode = hashCode(key);
         HashNode head = bucketArray.get(bucketIndex);
@@ -131,29 +172,40 @@ class Map {
                     headNode = headNode.next;
                 }
             }
-        }
+        }*/
     }
  
     public static void main(String[] args) {
     	//System.out.println("bababooey");
-        Map map = new Map("bababooey", 200);
+        HasMap map = new HasMap("bababooey", 200);
         long start;
         Random random = new Random();
         String endpointKey = null;
+        String value;
+        //HasMap temp = new HasMap(null, -1);
         
         for(int i = 0; i < 10000; i++) {
         	int num = random.nextInt(1000000);
         	//I'm making the key the code in this case for simplicity
         	map.add("" + num, "" + num);
+        	//System.out.println(map.get("" + num));
         	//
         	if(i == 100) {
         		endpointKey = "" + num;
         	}
         }
         
-        start = System.currentTimeMillis();
-        map.get(endpointKey);
-        System.out.println("Finished searching hash table in " + (System.currentTimeMillis() - start) + " milliseconds.");
+        for(int i = 0; i < 3; i++) {
+            start = System.currentTimeMillis();
+        	for(int j = 0; j < 100; j++) {
+        		value = Integer.toString(random.nextInt(10000));
+        		map.get(value);
+        	}
+            System.out.println("Finished " + (i + 1) + "st search of hash table in " + (System.currentTimeMillis() - start) + " milliseconds.");
+        }
+        
+        //start = System.currentTimeMillis();
+        //map.get(endpointKey);
         
         //map.add("bruh", 1);
         //System.out.println(map.remove("epic"));
